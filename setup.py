8a5e8c64ebfb855cd85eb17f02c0c52e7f6730b5
unni <aneeshcn01@gmail.com>
2021-04-07 15:22:59 +0200

    Adding the pre-commit settings

--- setup.py
+++ setup.py
@@ -18,15 +18,7 @@ setup(
     python_requires=">=3.8",
     extras_require={
         "dev": [
-            "bandit==1.6.2",
-            "black==19.10b0",
-            "flake8==3.8.3",
-            "flake8-bandit==2.1.2",
-            "flake8-bugbear==20.1.4",
-            "flake8-builtins==1.5.3",
-            "flake8-comprehensions==3.2.3",
-            "isort==5.5.2",
-            "pre-commit==2.6.0",
+            "pre-commit==2.12.0",
         ]
     },
 )
